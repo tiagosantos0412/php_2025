249c044a7abb056954d810695f1d28c633ce1b69
Tiago <tiagosantos0412@gmail.com>
2025-06-01 22:28:05 -0300

    Trabalhando com numeros inteiros.

---
+++ numeros_inteiros/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $idade = 38;
+     //concatenando numeros inteiros
+     echo $idade.$idade;
+    //operações aritméticas
+    echo '<p>Soma '.$idade .' + '.'10 = '.$idade + 10,'</p>'.
+         '<p>Subtração '.$idade .' - '.'10 = '.$idade - 10,'</p>'.
+         '<p>Multiplicação '.$idade .' x '.'10 = '.$idade * 10,'</p>'.
+         '<p>Divisão '.$idade .' / '.'10 = '.$idade / 10,'</p>'; 
+?>
